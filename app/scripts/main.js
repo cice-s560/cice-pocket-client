4fc611d15b9d80a7c6207ff037f7a4f93e45bd4a
GonzaloCedillo <goncedillo@gmail.com>
2018-11-03 14:58:47 +0100

    Usando filtros

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,11 @@
 M.AutoInit();
 
 // Self Invoked Function (contexto privado)
-(function () {
+(function() {
   // Expresión regular que comprueba una URL válida
-  const urlRegExp = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm);
+  const urlRegExp = new RegExp(
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
+  );
 
   const SERVER_URL = "http://localhost:3001";
 
@@ -15,16 +17,23 @@ M.AutoInit();
   const $websitesGrid = document.querySelector("#websites-grid");
   const $urlWebsite = document.querySelector("#url-website");
   const $submitWebsiteBtn = document.querySelector("#submit-website-btn");
+  const $categoryInput = document.querySelector("#category-input");
+  const $categorySelect = document.querySelector("#category-select");
 
   let isSendingWebsite = false;
-
 
   ////////////////////////////////////
   //// Helper functions
   ////
 
   // Card para pintar
-  const getCard = ({ title, url, description, image }) => `<div class="col s12 m6 l4">
+  const getCard = ({
+    title,
+    url,
+    description,
+    image,
+    category
+  }) => `<div class="col s12 m6 l4">
   <div class="card">
       <div class="card-image">
         <img src="${SERVER_URL}/${image}">
@@ -69,13 +78,58 @@ M.AutoInit();
     });
   }
 
-  async function getInitialCards() {
-    const req = await fetch(`${SERVER_URL}/website/list`).catch(err => websiteCreationFail());
+  async function getInitialCards(category) {
+    const categoryParam = (category && `?category=${category}`) || "";
+    const req = await fetch(`${SERVER_URL}/website/list${categoryParam}`).catch(
+      err => websiteCreationFail()
+    );
     const data = await req.json();
 
-    data.list && data.list.forEach(item => $websitesGrid.innerHTML += getCard(item));
+    $websitesGrid.innerHTML = "";
+
+    data.list &&
+      data.list.forEach(item => ($websitesGrid.innerHTML += getCard(item)));
   }
 
+  function requestCategories() {
+    return new Promise(async (resolve, reject) => {
+      const req = await fetch(`${SERVER_URL}/website/categories`).catch(err =>
+        reject(websiteCreationFail())
+      );
+      const data = await req.json();
+
+      resolve(data);
+    });
+  }
+
+  function refreshInputCategories(list) {
+    const dataToAutocomplete = {};
+    list.forEach(item => (dataToAutocomplete[item.name] = null));
+    M.Autocomplete.init($categoryInput, { data: dataToAutocomplete });
+  }
+
+  async function getInitialCategories() {
+    const data = await requestCategories();
+    // Traemos categorías y las pintamos en el select
+    data.categories &&
+      data.categories.forEach(
+        item =>
+          ($categorySelect.innerHTML += `<option value="${item._id}">${
+            item.name
+          }</option>`)
+      );
+
+    // Pasamos categorías al autocomplete
+    refreshInputCategories(data.categories);
+
+    // activamos el select para que renderice las categorías
+    M.FormSelect.init($categorySelect, { classes: "blanco" });
+
+    // Dar elevento de change
+    $categorySelect.addEventListener("change", e => {
+      getInitialCards(e.target.value);
+    });
+  }
 
   ////////////////////////////////////
   //// Handlers
@@ -119,11 +173,16 @@ M.AutoInit();
         "Content-Type": "application/json"
       },
       mode: "cors",
-      body: JSON.stringify({ url: $urlWebsite.value })
+      body: JSON.stringify({
+        url: $urlWebsite.value,
+        category: $categoryInput.value
+      })
     };
 
     // Envío + catch
-    const req = await fetch(`${SERVER_URL}/website/create`, reqOptions).catch(err => websiteCreationFail());
+    const req = await fetch(`${SERVER_URL}/website/create`, reqOptions).catch(
+      err => websiteCreationFail()
+    );
 
     // Si la respuesta no es creación, genera error
     if (req.status.toString() !== "201") {
@@ -133,14 +192,16 @@ M.AutoInit();
     // En caso de que todo vaya OK, sacamos el obj de respuesta
     const resp = await req.json();
 
+    // Refresh categories
+    getInitialCategories();
     // Generamos la card con los datos recibidos
-    websiteCreationSuccess(resp);
+    websiteCreationSuccess(resp[0]);
   });
-
 
   ///////////////////////////////////
   //// Scripts de inicio
   ////
 
   getInitialCards();
+  getInitialCategories();
 })();
